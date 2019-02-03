2f5854ef679da2689c2a9c7de12286e5e7fc0731
UnHa Kim <unha.kim.ghts@gmail.com>
2019-02-04 00:04:51 +0900

    t1102 자료형 단순화
    S거래원_정보 를 이용하여, 단순하게 변경.

--- tr_t1102.go
+++ tr_t1102.go
@@ -61,109 +61,110 @@ func F현물_시세_조회_t1102(종목코드 string) (응답값 *S현물_시세
 
 // t1102 현물 시세(현재가) 조회 응답
 type S현물_시세조회_응답 struct {
-	M한글명         string
-	M현재가         int64
-	M전일대비구분      T전일대비_구분
-	M전일대비등락폭     int64
-	M등락율         float64
-	M누적거래량       int64
-	M기준가         int64
-	M가중평균        int64
-	M상한가         int64
-	M하한가         int64
-	M전일거래량       int64
-	M거래량차        int64
-	M시가          int64
-	M시가시간        time.Time
-	M고가          int64
-	M고가시간        time.Time
-	M저가          int64
-	M저가시간        time.Time
-	M52주_최고가     int64
-	M52주_최고가일    time.Time
-	M52주_최저가     int64
-	M52주_최저가일    time.Time
-	M소진율         float64
-	PER          float64
-	PBRX         float64
-	M상장주식수_천     int64
-	M증거금율        int64
-	M수량단위        int64
-	M매도증권사코드_모음  []string
-	M매수증권사코드_모음  []string
-	M매도증권사명_모음   []string
-	M매수증권사명_모음   []string
-	M총매도수량_모음    []int64
-	M총매수수량_모음    []int64
-	M매도증감_모음     []int64
-	M매수증감_모음     []int64
-	M매도비율_모음     []float64
-	M매수비율_모음     []float64
-	M외국계_매도_합계수량 int64
-	M외국계_매도_직전대비 int64
-	M외국계_매도_비율   float64
-	M외국계_매수_합계수량 int64
-	M외국계_매수_직전대비 int64
-	M외국계_매수_비율   float64
-	M회전율         float64
-	M종목코드        string
-	M누적거래대금      int64
-	M전일동시간거래량    int64
-	M연중_최고가      int64
-	M연중_최고가_일자   time.Time
-	M연중_최저가      int64
-	M연중_최저가_일자   time.Time
-	M목표가         int64
-	M자본금         int64
-	M유동주식수       int64
-	M액면가         int64
-	M결산월         uint8
-	M대용가         int64
-	M시가총액_억      int64
-	M상장일         time.Time
-	M전분기명        string
-	M전분기_매출액     int64
-	M전분기_영업이익    int64
-	M전분기_경상이익    int64
-	M전분기_순이익     int64
-	M전분기EPS      float64
-	M전전분기명       string
-	M전전분기_매출액    int64
-	M전전분기_영업이익   int64
-	M전전분기_경상이익   int64
-	M전전분기_순이익    int64
-	M전전분기EPS     float64
-	M전년대비매출액     float64
-	M전년대비영업이익    float64
-	M전년대비경상이익    float64
-	M전년대비순이익     float64
-	M전년대비EPS     float64
-	M락구분         string
-	M관리_급등구분     string
-	M정지_연장구분     string
-	M투자_불성실구분    string
-	M시장구분        lib.T시장구분
-	T_PER        float64
-	M통화ISO코드     string
-	M총매도대금_모음    []int64
-	M총매수대금_모음    []int64
-	M총매도평단가_모음   []int64
-	M총매수평단가_모음   []int64
-	M외국계매도대금     int64
-	M외국계매수대금     int64
-	M외국계매도평단가    int64
-	M외국계매수평단가    int64
-	M투자주의환기      string
-	M기업인수목적회사여부  bool
-	M발행가격        int64
-	M배분적용구분코드    string // 배분적용구분코드(1:배분발생 2:배)
-	M배분적용구분      string
-	M단기과열_VI발동   string
-	M정적VI상한가     int64
-	M정적VI하한가     int64
-	M저유동성종목여부    bool
-	M이상급등종목여부    bool
-	M대차불가여부      bool
+	M종목코드     string
+	M일자       time.Time
+	M종목명      string
+	M현재가      int64
+	M전일대비구분   T전일대비_구분
+	M전일대비등락폭  int64
+	M등락율      float64
+	M거래량      int64
+	M기준가      int64
+	M가중평균     int64
+	M상한가      int64
+	M하한가      int64
+	M전일거래량    int64
+	M거래량차     int64
+	M시가       int64
+	M시가시간     time.Time
+	M고가       int64
+	M고가시간     time.Time
+	M저가       int64
+	M저가시간     time.Time
+	M52주_최고가  int64
+	M52주_최고가일 time.Time
+	M52주_최저가  int64
+	M52주_최저가일 time.Time
+	M소진율      float64
+	PER            float64
+	PBR            float64
+	M상장주식수_천       int64
+	M증거금율          int64
+	M수량단위          int64
+	M회전율           float64
+	M거래대금_백만       int64
+	M전일동시간거래량      int64
+	M연중_최고가        int64
+	M연중_최고가_일자     time.Time
+	M연중_최저가        int64
+	M연중_최저가_일자     time.Time
+	M목표가           int64
+	M자본금_억         int64
+	M유동주식수_천       int64
+	M액면가           int64
+	M결산월           uint8
+	M대용가           int64
+	M시가총액_억        int64
+	M상장일           time.Time
+	M전분기명          string
+	M전분기_매출액_억     int64
+	M전분기_영업이익_억    int64
+	M전분기_경상이익_억    int64
+	M전분기_순이익_억     int64
+	M전분기EPS        float64
+	M전전분기명         string
+	M전전분기_매출액      int64
+	M전전분기_영업이익     int64
+	M전전분기_경상이익     int64
+	M전전분기_순이익      int64
+	M전전분기EPS       float64
+	M전년대비_매출액_증감율  float64
+	M전년대비_영업이익_증감율 float64
+	M전년대비_경상이익_증감율 float64
+	M전년대비_순이익_증감율  float64
+	M전년대비_EPS_증감율  float64
+	M락구분           string // 권배락, 권리락, 배당락, 액면분할, 액면병합, 주식병합, 기업분할, 감자
+	M관리_급등구분       string // 관리/경고, 관리/위험, 관리, 예고, 경고, 위험
+	M정지_연장구분       string
+	M투자_불성실구분      string
+	M시장구분          lib.T시장구분
+	T_PER          float64
+	M통화ISO코드       string
+	M투자주의환기        string
+	M기업인수목적회사여부    bool
+	M발행가격          int64
+	M배분적용구분코드      string // 배분적용구분코드(1:배분발생 2:배)
+	M배분적용구분        string
+	M단기과열_VI발동     string
+	M정적VI상한가       int64
+	M정적VI하한가       int64
+	M저유동성종목여부      bool
+	M이상급등종목여부      bool
+	M대차불가여부        bool
+	M매도_거래원_정보_모음	[]*S거래원_정보_t1102
+	M매수_거래원_정보_모음	[]*S거래원_정보_t1102
+	M외국계_매도_거래원_정보 *S거래원_정보_t1102
+	M외국계_매수_거래원_정보 *S거래원_정보_t1102
+}
+
+type S거래원_정보_t1102 struct {
+	M증권사_코드  string
+	M이름      string
+	M거래_수량   int64
+	M평균_단가   int64
+	M거래_대금   int64
+	M전일대비_증감 int64
+	M비율      float64
+}
+
+func New거래원_정보_모음_t1102(수량 int) []*S거래원_정보_t1102 {
+	거래원_정보_모음 := make([]*S거래원_정보_t1102, 수량)
+
+	for i:=0 ; i<수량 ; i++ {
+		거래원_정보_모음[i] = new(S거래원_정보_t1102)
+	}
+
+	return 거래원_정보_모음
 }
 
 func New현물_시세조회_응답(b []byte) (s *S현물_시세조회_응답, 에러 error) {
@@ -177,12 +178,13 @@ func New현물_시세조회_응답(b []byte) (s *S현물_시세조회_응답, �
 
 	s = new(S현물_시세조회_응답)
 	s.M종목코드 = lib.F2문자열_공백제거(g.Shcode)
-	s.M한글명 = lib.F2문자열_EUC_KR(g.Hname)
+	s.M일자 = F당일()
+	s.M종목명 = lib.F2문자열_EUC_KR(g.Hname)
 	s.M현재가 = lib.F2정수64_단순형(g.Price)
 	s.M전일대비구분 = T전일대비_구분(lib.F2정수64_단순형(g.Sign))
 	s.M전일대비등락폭 = lib.F2정수64_단순형(g.Change)
 	s.M등락율 = lib.F2실수_소숫점_추가_단순형(g.Diff, 2)
-	s.M누적거래량 = lib.F2정수64_단순형(g.Volume)
+	s.M거래량 = lib.F2정수64_단순형(g.Volume)
 	s.M기준가 = lib.F2정수64_단순형(g.Recprice)
 	s.M가중평균 = lib.F2정수64_단순형(g.Avg)
 	s.M상한가 = lib.F2정수64_단순형(g.Uplmtprice)
@@ -201,96 +203,30 @@ func New현물_시세조회_응답(b []byte) (s *S현물_시세조회_응답, �
 	s.M52주_최저가일 = lib.F2포맷된_시각_단순형_공백은_초기값("20060102", g.Low52wdate)
 	s.M소진율 = lib.F2실수_소숫점_추가_단순형(g.Exhratio, 2)
 	s.PER = lib.F2실수_소숫점_추가_단순형(g.Per, 2)
-	s.PBRX = lib.F2실수_소숫점_추가_단순형(g.Pbrx, 2)
+	s.PBR = lib.F2실수_소숫점_추가_단순형(g.Pbrx, 2)
 	s.M상장주식수_천 = lib.F2정수64_단순형(g.Listing)
 	s.M증거금율 = lib.F2정수64_단순형(g.Jkrate)
 	s.M수량단위 = lib.F2정수64_단순형(g.Memedan)
-	s.M매도증권사코드_모음 = make([]string, 5)
-	s.M매수증권사코드_모음 = make([]string, 5)
-	s.M매도증권사명_모음 = make([]string, 5)
-	s.M매수증권사명_모음 = make([]string, 5)
-	s.M총매도수량_모음 = make([]int64, 5)
-	s.M총매수수량_모음 = make([]int64, 5)
-	s.M매도증감_모음 = make([]int64, 5)
-	s.M매수증감_모음 = make([]int64, 5)
-	s.M매도비율_모음 = make([]float64, 5)
-	s.M매수비율_모음 = make([]float64, 5)
-	s.M매도증권사코드_모음[0] = lib.F2문자열_공백제거(g.Offernocd1)
-	s.M매수증권사코드_모음[0] = lib.F2문자열_공백제거(g.Bidnocd1)
-	s.M매도증권사명_모음[0] = lib.F2문자열_EUC_KR(g.Offerno1)
-	s.M매수증권사명_모음[0] = lib.F2문자열_EUC_KR(g.Bidno1)
-	s.M총매도수량_모음[0] = lib.F2정수64_단순형(g.Dvol1)
-	s.M총매수수량_모음[0] = lib.F2정수64_단순형(g.Svol1)
-	s.M매도증감_모음[0] = lib.F2정수64_단순형(g.Dcha1)
-	s.M매수증감_모음[0] = lib.F2정수64_단순형(g.Scha1)
-	s.M매도비율_모음[0] = lib.F2실수_소숫점_추가_단순형(g.Ddiff1, 2)
-	s.M매수비율_모음[0] = lib.F2실수_소숫점_추가_단순형(g.Sdiff1, 2)
-	s.M매도증권사코드_모음[1] = lib.F2문자열_공백제거(g.Offernocd2)
-	s.M매수증권사코드_모음[1] = lib.F2문자열_공백제거(g.Bidnocd2)
-	s.M매도증권사명_모음[1] = lib.F2문자열_EUC_KR(g.Offerno2)
-	s.M매수증권사명_모음[1] = lib.F2문자열_EUC_KR(g.Bidno2)
-	s.M총매도수량_모음[1] = lib.F2정수64_단순형(g.Dvol2)
-	s.M총매수수량_모음[1] = lib.F2정수64_단순형(g.Svol2)
-	s.M매도증감_모음[1] = lib.F2정수64_단순형(g.Dcha2)
-	s.M매수증감_모음[1] = lib.F2정수64_단순형(g.Scha2)
-	s.M매도비율_모음[1] = lib.F2실수_소숫점_추가_단순형(g.Ddiff2, 2)
-	s.M매수비율_모음[1] = lib.F2실수_소숫점_추가_단순형(g.Sdiff2, 2)
-	s.M매도증권사코드_모음[2] = lib.F2문자열_공백제거(g.Offernocd3)
-	s.M매수증권사코드_모음[2] = lib.F2문자열_공백제거(g.Bidnocd3)
-	s.M매도증권사명_모음[2] = lib.F2문자열_EUC_KR(g.Offerno3)
-	s.M매수증권사명_모음[2] = lib.F2문자열_EUC_KR(g.Bidno3)
-	s.M총매도수량_모음[2] = lib.F2정수64_단순형(g.Dvol3)
-	s.M총매수수량_모음[2] = lib.F2정수64_단순형(g.Svol3)
-	s.M매도증감_모음[2] = lib.F2정수64_단순형(g.Dcha3)
-	s.M매수증감_모음[2] = lib.F2정수64_단순형(g.Scha3)
-	s.M매도비율_모음[2] = lib.F2실수_소숫점_추가_단순형(g.Ddiff3, 2)
-	s.M매수비율_모음[2] = lib.F2실수_소숫점_추가_단순형(g.Sdiff3, 2)
-	s.M매도증권사코드_모음[3] = lib.F2문자열_공백제거(g.Offernocd4)
-	s.M매수증권사코드_모음[3] = lib.F2문자열_공백제거(g.Bidnocd4)
-	s.M매도증권사명_모음[3] = lib.F2문자열_EUC_KR(g.Offerno4)
-	s.M매수증권사명_모음[3] = lib.F2문자열_EUC_KR(g.Bidno4)
-	s.M총매도수량_모음[3] = lib.F2정수64_단순형(g.Dvol4)
-	s.M총매수수량_모음[3] = lib.F2정수64_단순형(g.Svol4)
-	s.M매도증감_모음[3] = lib.F2정수64_단순형(g.Dcha4)
-	s.M매수증감_모음[3] = lib.F2정수64_단순형(g.Scha4)
-	s.M매도비율_모음[3] = lib.F2실수_소숫점_추가_단순형(g.Ddiff4, 2)
-	s.M매수비율_모음[3] = lib.F2실수_소숫점_추가_단순형(g.Sdiff4, 2)
-	s.M매도증권사코드_모음[4] = lib.F2문자열_공백제거(g.Offernocd5)
-	s.M매수증권사코드_모음[4] = lib.F2문자열_공백제거(g.Bidnocd5)
-	s.M매도증권사명_모음[4] = lib.F2문자열_EUC_KR(g.Offerno5)
-	s.M매수증권사명_모음[4] = lib.F2문자열_EUC_KR(g.Bidno5)
-	s.M총매도수량_모음[4] = lib.F2정수64_단순형(g.Dvol5)
-	s.M총매수수량_모음[4] = lib.F2정수64_단순형(g.Svol5)
-	s.M매도증감_모음[4] = lib.F2정수64_단순형(g.Dcha5)
-	s.M매수증감_모음[4] = lib.F2정수64_단순형(g.Scha5)
-	s.M매도비율_모음[4] = lib.F2실수_소숫점_추가_단순형(g.Ddiff5, 2)
-	s.M매수비율_모음[4] = lib.F2실수_소숫점_추가_단순형(g.Sdiff5, 2)
-	s.M외국계_매도_합계수량 = lib.F2정수64_단순형(g.Fwdvl)
-	s.M외국계_매도_직전대비 = lib.F2정수64_단순형(g.Ftradmdcha)
-	s.M외국계_매도_비율 = lib.F2실수_소숫점_추가_단순형(g.Ftradmddiff, 2)
-	s.M외국계_매수_합계수량 = lib.F2정수64_단순형(g.Fwsvl)
-	s.M외국계_매수_직전대비 = lib.F2정수64_단순형(g.Ftradmscha)
-	s.M외국계_매수_비율 = lib.F2실수_소숫점_추가_단순형(g.Ftradmsdiff, 2)
 	s.M회전율 = lib.F2실수_소숫점_추가_단순형(g.Vol, 2)
-	s.M누적거래대금 = lib.F2정수64_단순형(g.Value)
+	s.M거래대금_백만 = lib.F2정수64_단순형(g.Value)
 	s.M전일동시간거래량 = lib.F2정수64_단순형(g.Jvolume)
 	s.M연중_최고가 = lib.F2정수64_단순형(g.Highyear)
 	s.M연중_최고가_일자 = lib.F2포맷된_시각_단순형("20060102", g.Highyeardate)
 	s.M연중_최저가 = lib.F2정수64_단순형(g.Lowyear)
 	s.M연중_최저가_일자 = lib.F2포맷된_시각_단순형("20060102", g.Lowyeardate)
 	s.M목표가 = lib.F2정수64_단순형(g.Target)
-	s.M자본금 = lib.F2정수64_단순형(g.Capital)
-	s.M유동주식수 = lib.F2정수64_단순형(g.Abscnt)
+	s.M자본금_억 = lib.F2정수64_단순형(g.Capital)
+	s.M유동주식수_천 = lib.F2정수64_단순형(g.Abscnt)
 	s.M액면가 = lib.F2정수64_단순형(g.Parprice)
 	s.M결산월 = uint8(lib.F2정수64_단순형_공백은_0(g.Gsmm))
 	s.M대용가 = lib.F2정수64_단순형(g.Subprice)
 	s.M시가총액_억 = lib.F2정수64_단순형(g.Total)
 	s.M상장일 = lib.F2포맷된_시각_단순형("20060102", g.Listdate)
 	s.M전분기명 = lib.F2문자열_EUC_KR_공백제거(g.Name)
-	s.M전분기_매출액 = lib.F2정수64_단순형(g.Bfsales)
-	s.M전분기_영업이익 = lib.F2정수64_단순형(g.Bfoperatingincome)
-	s.M전분기_경상이익 = lib.F2정수64_단순형(g.Bfordinaryincome)
-	s.M전분기_순이익 = lib.F2정수64_단순형(g.Bfnetincome)
+	s.M전분기_매출액_억 = lib.F2정수64_단순형(g.Bfsales)
+	s.M전분기_영업이익_억 = lib.F2정수64_단순형(g.Bfoperatingincome)
+	s.M전분기_경상이익_억 = lib.F2정수64_단순형(g.Bfordinaryincome)
+	s.M전분기_순이익_억 = lib.F2정수64_단순형(g.Bfnetincome)
 	s.M전분기EPS = lib.F2실수_소숫점_추가_단순형(g.Bfeps, 2)
 	s.M전전분기명 = lib.F2문자열_EUC_KR_공백제거(g.Name2)
 	s.M전전분기_매출액 = lib.F2정수64_단순형(g.Bfsales2)
@@ -298,47 +234,18 @@ func New현물_시세조회_응답(b []byte) (s *S현물_시세조회_응답, �
 	s.M전전분기_경상이익 = lib.F2정수64_단순형(g.Bfordinaryincome2)
 	s.M전전분기_순이익 = lib.F2정수64_단순형(g.Bfnetincome2)
 	s.M전전분기EPS = lib.F2실수_소숫점_추가_단순형(g.Bfeps2, 2)
-	s.M전년대비매출액 = lib.F2실수_소숫점_추가_단순형(g.Salert, 2)
-	s.M전년대비영업이익 = lib.F2실수_소숫점_추가_단순형(g.Opert, 2)
-	s.M전년대비경상이익 = lib.F2실수_소숫점_추가_단순형(g.Ordrt, 2)
-	s.M전년대비순이익 = lib.F2실수_소숫점_추가_단순형(g.Netrt, 2)
-	s.M전년대비EPS = lib.F2실수_소숫점_추가_단순형(g.Epsrt, 2)
-	s.M락구분 = lib.F2문자열_EUC_KR(g.Info1)
-	s.M관리_급등구분 = lib.F2문자열_EUC_KR(g.Info2)
-	s.M정지_연장구분 = lib.F2문자열_EUC_KR(g.Info3)
-	s.M투자_불성실구분 = lib.F2문자열_EUC_KR(g.Info4)
+	s.M전년대비_매출액_증감율 = lib.F2실수_소숫점_추가_단순형(g.Salert, 2)
+	s.M전년대비_영업이익_증감율 = lib.F2실수_소숫점_추가_단순형(g.Opert, 2)
+	s.M전년대비_경상이익_증감율 = lib.F2실수_소숫점_추가_단순형(g.Ordrt, 2)
+	s.M전년대비_순이익_증감율 = lib.F2실수_소숫점_추가_단순형(g.Netrt, 2)
+	s.M전년대비_EPS_증감율 = lib.F2실수_소숫점_추가_단순형(g.Epsrt, 2)
+	s.M락구분 = lib.F2문자열_EUC_KR_공백제거(g.Info1)     // 권배락, 권리락, 배당락, 액면분할, 액면병합, 주식병합, 기업분할, 감자
+	s.M관리_급등구분 = lib.F2문자열_EUC_KR_공백제거(g.Info2) // 관리/경고, 관리/위험, 관리, 예고, 경고, 위험
+	s.M정지_연장구분 = lib.F2문자열_EUC_KR_공백제거(g.Info3) // 거래정지, 거래중단, 시가연장, 종가연장
+	s.M투자_불성실구분 = lib.F2문자열_EUC_KR_공백제거(g.Info4)
 	s.M시장구분 = f2시장구분(g.Janginfo)
 	s.T_PER = lib.F2실수_소숫점_추가_단순형(g.T_per, 2)
 	s.M통화ISO코드 = lib.F2문자열_공백제거(g.Tonghwa)
-	s.M총매도대금_모음 = make([]int64, 5)
-	s.M총매수대금_모음 = make([]int64, 5)
-	s.M총매도대금_모음[0] = lib.F2정수64_단순형_공백은_0(g.Dval1)
-	s.M총매수대금_모음[0] = lib.F2정수64_단순형_공백은_0(g.Sval1)
-	s.M총매도대금_모음[1] = lib.F2정수64_단순형_공백은_0(g.Dval2)
-	s.M총매수대금_모음[1] = lib.F2정수64_단순형_공백은_0(g.Sval2)
-	s.M총매도대금_모음[2] = lib.F2정수64_단순형_공백은_0(g.Dval3)
-	s.M총매수대금_모음[2] = lib.F2정수64_단순형_공백은_0(g.Sval3)
-	s.M총매도대금_모음[3] = lib.F2정수64_단순형_공백은_0(g.Dval4)
-	s.M총매수대금_모음[3] = lib.F2정수64_단순형_공백은_0(g.Sval4)
-	s.M총매도대금_모음[4] = lib.F2정수64_단순형_공백은_0(g.Dval5)
-	s.M총매수대금_모음[4] = lib.F2정수64_단순형_공백은_0(g.Sval5)
-	s.M총매도평단가_모음 = make([]int64, 5)
-	s.M총매수평단가_모음 = make([]int64, 5)
-	s.M총매도평단가_모음[0] = lib.F2정수64_단순형_공백은_0(g.Davg1)
-	s.M총매수평단가_모음[0] = lib.F2정수64_단순형_공백은_0(g.Savg1)
-	s.M총매도평단가_모음[1] = lib.F2정수64_단순형_공백은_0(g.Davg2)
-	s.M총매수평단가_모음[1] = lib.F2정수64_단순형_공백은_0(g.Savg2)
-	s.M총매도평단가_모음[2] = lib.F2정수64_단순형_공백은_0(g.Davg3)
-	s.M총매수평단가_모음[2] = lib.F2정수64_단순형_공백은_0(g.Savg3)
-	s.M총매도평단가_모음[3] = lib.F2정수64_단순형_공백은_0(g.Davg4)
-	s.M총매수평단가_모음[3] = lib.F2정수64_단순형_공백은_0(g.Savg4)
-	s.M총매도평단가_모음[4] = lib.F2정수64_단순형_공백은_0(g.Davg5)
-	s.M총매수평단가_모음[4] = lib.F2정수64_단순형_공백은_0(g.Savg5)
-	s.M외국계매도대금 = lib.F2정수64_단순형(g.Ftradmdval)
-	s.M외국계매수대금 = lib.F2정수64_단순형_공백은_0(g.Ftradmsval)
-	s.M외국계매도평단가 = lib.F2정수64_단순형_공백은_0(g.Ftradmdavg)
-	s.M외국계매도평단가 = lib.F2정수64_단순형_공백은_0(g.Ftradmdavg)
-	s.M외국계매수평단가 = lib.F2정수64_단순형_공백은_0(g.Ftradmsavg)
 	s.M투자주의환기 = lib.F2문자열_EUC_KR_공백제거(g.Info5)
 	s.M기업인수목적회사여부 = lib.F2참거짓(g.Spac_gubun, "N", false)
 	s.M발행가격 = lib.F2정수64_단순형(g.Issueprice)
@@ -350,6 +257,7 @@ func New현물_시세조회_응답(b []byte) (s *S현물_시세조회_응답, �
 	s.M저유동성종목여부 = lib.F2참거짓(g.Low_lqdt_gu, 1, true)
 	s.M이상급등종목여부 = lib.F2참거짓(g.Abnormal_rise_gu, 1, true)
 
+
 	대차불가표시_문자열 := lib.F2문자열_EUC_KR_공백제거(g.Lend_text)
 	switch 대차불가표시_문자열 {
 	case "":
@@ -359,6 +267,104 @@ func New현물_시세조회_응답(b []byte) (s *S현물_시세조회_응답, �
 	default:
 		panic(lib.New에러("%v '대차불가표시_문자열' 예상하지 못한 값 : '%v'", s.M종목코드, 대차불가표시_문자열))
 	}
+
+	s.M매도_거래원_정보_모음 = New거래원_정보_모음_t1102(5)
+
+	s.M매도_거래원_정보_모음[0].M증권사_코드 = lib.F2문자열_공백제거(g.Offernocd1)
+	s.M매도_거래원_정보_모음[0].M이름 = lib.F2문자열_EUC_KR(g.Offerno1)
+	s.M매도_거래원_정보_모음[0].M거래_수량 = lib.F2정수64_단순형(g.Dvol1)
+	s.M매도_거래원_정보_모음[0].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Davg1)
+	s.M매도_거래원_정보_모음[0].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Dval1)
+	s.M매도_거래원_정보_모음[0].M전일대비_증감 = lib.F2정수64_단순형(g.Dcha1)
+	s.M매도_거래원_정보_모음[0].M비율 = lib.F2실수_소숫점_추가_단순형(g.Ddiff1, 2)
+
+	s.M매도_거래원_정보_모음[1].M증권사_코드 = lib.F2문자열_공백제거(g.Offernocd2)
+	s.M매도_거래원_정보_모음[1].M이름 = lib.F2문자열_EUC_KR(g.Offerno2)
+	s.M매도_거래원_정보_모음[1].M거래_수량 = lib.F2정수64_단순형(g.Dvol2)
+	s.M매도_거래원_정보_모음[1].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Davg2)
+	s.M매도_거래원_정보_모음[1].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Dval2)
+	s.M매도_거래원_정보_모음[1].M전일대비_증감 = lib.F2정수64_단순형(g.Dcha2)
+	s.M매도_거래원_정보_모음[1].M비율 = lib.F2실수_소숫점_추가_단순형(g.Ddiff2, 2)
+
+	s.M매도_거래원_정보_모음[2].M증권사_코드 = lib.F2문자열_공백제거(g.Offernocd3)
+	s.M매도_거래원_정보_모음[2].M이름 = lib.F2문자열_EUC_KR(g.Offerno3)
+	s.M매도_거래원_정보_모음[2].M거래_수량 = lib.F2정수64_단순형(g.Dvol3)
+	s.M매도_거래원_정보_모음[2].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Davg3)
+	s.M매도_거래원_정보_모음[2].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Dval3)
+	s.M매도_거래원_정보_모음[2].M전일대비_증감 = lib.F2정수64_단순형(g.Dcha3)
+	s.M매도_거래원_정보_모음[2].M비율 = lib.F2실수_소숫점_추가_단순형(g.Ddiff3, 2)
+
+	s.M매도_거래원_정보_모음[3].M증권사_코드 = lib.F2문자열_공백제거(g.Offernocd4)
+	s.M매도_거래원_정보_모음[3].M이름 = lib.F2문자열_EUC_KR(g.Offerno4)
+	s.M매도_거래원_정보_모음[3].M거래_수량 = lib.F2정수64_단순형(g.Dvol4)
+	s.M매도_거래원_정보_모음[3].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Davg4)
+	s.M매도_거래원_정보_모음[3].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Dval4)
+	s.M매도_거래원_정보_모음[3].M전일대비_증감 = lib.F2정수64_단순형(g.Dcha4)
+	s.M매도_거래원_정보_모음[3].M비율 = lib.F2실수_소숫점_추가_단순형(g.Ddiff4, 2)
+
+	s.M매도_거래원_정보_모음[4].M증권사_코드 = lib.F2문자열_공백제거(g.Offernocd5)
+	s.M매도_거래원_정보_모음[4].M이름 = lib.F2문자열_EUC_KR(g.Offerno5)
+	s.M매도_거래원_정보_모음[4].M거래_수량 = lib.F2정수64_단순형(g.Dvol5)
+	s.M매도_거래원_정보_모음[4].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Davg5)
+	s.M매도_거래원_정보_모음[4].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Dval5)
+	s.M매도_거래원_정보_모음[4].M전일대비_증감 = lib.F2정수64_단순형(g.Dcha5)
+	s.M매도_거래원_정보_모음[4].M비율 = lib.F2실수_소숫점_추가_단순형(g.Ddiff5, 2)
+
+	s.M매수_거래원_정보_모음 = New거래원_정보_모음_t1102(5)
+
+	s.M매수_거래원_정보_모음[0].M증권사_코드 = lib.F2문자열_공백제거(g.Bidnocd1)
+	s.M매수_거래원_정보_모음[0].M이름 = lib.F2문자열_EUC_KR(g.Bidno1)
+	s.M매수_거래원_정보_모음[0].M거래_수량 = lib.F2정수64_단순형(g.Svol1)
+	s.M매수_거래원_정보_모음[0].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Savg1)
+	s.M매수_거래원_정보_모음[0].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Sval1)
+	s.M매수_거래원_정보_모음[0].M전일대비_증감 = lib.F2정수64_단순형(g.Scha1)
+	s.M매수_거래원_정보_모음[0].M비율 = lib.F2실수_소숫점_추가_단순형(g.Sdiff1, 2)
+
+	s.M매수_거래원_정보_모음[1].M증권사_코드 = lib.F2문자열_공백제거(g.Bidnocd2)
+	s.M매수_거래원_정보_모음[1].M이름 = lib.F2문자열_EUC_KR(g.Bidno2)
+	s.M매수_거래원_정보_모음[1].M거래_수량 = lib.F2정수64_단순형(g.Svol2)
+	s.M매수_거래원_정보_모음[1].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Savg2)
+	s.M매수_거래원_정보_모음[1].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Sval2)
+	s.M매수_거래원_정보_모음[1].M전일대비_증감 = lib.F2정수64_단순형(g.Scha2)
+	s.M매수_거래원_정보_모음[1].M비율 = lib.F2실수_소숫점_추가_단순형(g.Sdiff2, 2)
+
+	s.M매수_거래원_정보_모음[2].M증권사_코드 = lib.F2문자열_공백제거(g.Bidnocd3)
+	s.M매수_거래원_정보_모음[2].M이름 = lib.F2문자열_EUC_KR(g.Bidno3)
+	s.M매수_거래원_정보_모음[2].M거래_수량 = lib.F2정수64_단순형(g.Svol3)
+	s.M매수_거래원_정보_모음[2].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Savg3)
+	s.M매수_거래원_정보_모음[2].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Sval3)
+	s.M매수_거래원_정보_모음[2].M전일대비_증감 = lib.F2정수64_단순형(g.Scha3)
+	s.M매수_거래원_정보_모음[2].M비율 = lib.F2실수_소숫점_추가_단순형(g.Sdiff3, 2)
+
+	s.M매수_거래원_정보_모음[3].M증권사_코드 = lib.F2문자열_공백제거(g.Bidnocd4)
+	s.M매수_거래원_정보_모음[3].M이름 = lib.F2문자열_EUC_KR(g.Bidno4)
+	s.M매수_거래원_정보_모음[3].M거래_수량 = lib.F2정수64_단순형(g.Svol4)
+	s.M매수_거래원_정보_모음[3].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Savg4)
+	s.M매수_거래원_정보_모음[3].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Sval4)
+	s.M매수_거래원_정보_모음[3].M전일대비_증감 = lib.F2정수64_단순형(g.Scha4)
+	s.M매수_거래원_정보_모음[3].M비율 = lib.F2실수_소숫점_추가_단순형(g.Sdiff4, 2)
+
+	s.M매수_거래원_정보_모음[4].M증권사_코드 = lib.F2문자열_공백제거(g.Bidnocd5)
+	s.M매수_거래원_정보_모음[4].M이름 = lib.F2문자열_EUC_KR(g.Bidno5)
+	s.M매수_거래원_정보_모음[4].M거래_수량 = lib.F2정수64_단순형(g.Svol5)
+	s.M매수_거래원_정보_모음[4].M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Savg5)
+	s.M매수_거래원_정보_모음[4].M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Sval5)
+	s.M매수_거래원_정보_모음[4].M전일대비_증감 = lib.F2정수64_단순형(g.Scha5)
+	s.M매수_거래원_정보_모음[4].M비율 = lib.F2실수_소숫점_추가_단순형(g.Sdiff5, 2)
+
+	s.M외국계_매도_거래원_정보 = new(S거래원_정보_t1102)
+	s.M외국계_매도_거래원_정보.M거래_수량 = lib.F2정수64_단순형(g.Fwdvl)
+	s.M외국계_매도_거래원_정보.M평균_단가 = lib.F2정수64_단순형_공백은_0(g.Ftradmdavg)
+	s.M외국계_매도_거래원_정보.M거래_대금 = lib.F2정수64_단순형(g.Ftradmdval)
+	s.M외국계_매도_거래원_정보.M전일대비_증감 = lib.F2정수64_단순형(g.Ftradmdcha)
+	s.M외국계_매도_거래원_정보.M비율 = lib.F2실수_소숫점_추가_단순형(g.Ftradmddiff, 2)
+
+	s.M외국계_매수_거래원_정보 = new(S거래원_정보_t1102)
+	s.M외국계_매수_거래원_정보.M거래_수량 = lib.F2정수64_단순형(g.Fwsvl)
+	s.M외국계_매수_거래원_정보.M평균_단가 =  lib.F2정수64_단순형_공백은_0(g.Ftradmsavg)
+	s.M외국계_매수_거래원_정보.M거래_대금 = lib.F2정수64_단순형_공백은_0(g.Ftradmsval)
+	s.M외국계_매수_거래원_정보.M전일대비_증감 = lib.F2정수64_단순형(g.Ftradmscha)
+	s.M외국계_매수_거래원_정보.M비율 = lib.F2실수_소숫점_추가_단순형(g.Ftradmsdiff, 2)
 
 	return s, nil
 }
