6abe3453bc7e57f0d18bc5f3ebdeb75232e3fdd5
UnHa Kim <unha.kim.ghts@gmail.com>
2019-02-05 17:00:27 +0900

    t1404 소스 코드 정리

--- tr_t1404.go
+++ tr_t1404.go
@@ -155,8 +155,8 @@ func New관리종목_조회_응답_헤더_t1404(b []byte) (값 *S관리종목_�
 	return 값, nil
 }
 
-func New관리종목_조회_응답_반복값_모음_t1404(b []byte) (값 *S관리종목_조회_응답_반복값_모음_t1404, 에러 error) {
-	defer lib.S예외처리{M에러: &에러, M함수: func() { 값 = nil }}.S실행()
+func New관리종목_조회_응답_반복값_모음_t1404(b []byte) (값_모음 *S관리종목_조회_응답_반복값_모음_t1404, 에러 error) {
+	defer lib.S예외처리{M에러: &에러, M함수: func() { 값_모음 = nil }}.S실행()
 
 	나머지 := len(b) % SizeT1404OutBlock1
 	lib.F조건부_패닉(나머지 != 0, "예상하지 못한 길이. '%v' '%v'", len(b), 나머지)
@@ -165,30 +165,30 @@ func New관리종목_조회_응답_반복값_모음_t1404(b []byte) (값 *S관�
 	수량 := len(b) / SizeT1404OutBlock1
 	g_모음 := make([]*T1404OutBlock1, 수량, 수량)
 
-	값 = new(S관리종목_조회_응답_반복값_모음_t1404)
-	값.M배열 = make([]*S관리종목_조회_응답_반복값_t1404, 수량, 수량)
+	값_모음 = new(S관리종목_조회_응답_반복값_모음_t1404)
+	값_모음.M배열 = make([]*S관리종목_조회_응답_반복값_t1404, 수량, 수량)
 
 	for i, g := range g_모음 {
 		g = new(T1404OutBlock1)
 		lib.F확인(binary.Read(버퍼, binary.BigEndian, g))
 
-		s := new(S관리종목_조회_응답_반복값_t1404)
-		s.M종목코드 = lib.F2문자열(g.Shcode)
-		s.M종목명 = lib.F2문자열_EUC_KR_공백제거(g.Hname)
-		s.M현재가 = lib.F2정수64_단순형(g.Price)
-		s.M전일대비구분 = T전일대비_구분(lib.F2정수64_단순형(g.Sign))
-		s.M전일대비_등락폭 = s.M전일대비구분.G부호보정_정수64(lib.F2정수64_단순형(g.Change))
-		s.M전일대비_등락율 = s.M전일대비구분.G부호보정_실수64(lib.F2실수_소숫점_추가_단순형(g.Diff, 2))
-		s.M거래량 = lib.F2정수64_단순형(g.Volume)
-		s.M지정일 = lib.F2포맷된_일자_단순형("20060102", g.Date)
-		s.M지정일_주가 = lib.F2정수64_단순형(g.Tprice)
-		s.M지정일_대비_등락폭 = lib.F2정수64_단순형(g.Tchange)
-		s.M지정일_대비_등락율 = lib.F2실수_소숫점_추가_단순형(g.Tdiff, 2)
-		s.M사유 = T관리종목_지정_사유_구분(lib.F2정수64_단순형_공백은_0(g.Reason))
-		s.M해제일 = lib.F2포맷된_일자_단순형_공백은_초기값("20060102", g.Edate)
+		값 := new(S관리종목_조회_응답_반복값_t1404)
+		값.M종목코드 = lib.F2문자열(g.Shcode)
+		값.M종목명 = lib.F2문자열_EUC_KR_공백제거(g.Hname)
+		값.M현재가 = lib.F2정수64_단순형(g.Price)
+		값.M전일대비구분 = T전일대비_구분(lib.F2정수64_단순형(g.Sign))
+		값.M전일대비_등락폭 = 값.M전일대비구분.G부호보정_정수64(lib.F2정수64_단순형(g.Change))
+		값.M전일대비_등락율 = 값.M전일대비구분.G부호보정_실수64(lib.F2실수_소숫점_추가_단순형(g.Diff, 2))
+		값.M거래량 = lib.F2정수64_단순형(g.Volume)
+		값.M지정일 = lib.F2포맷된_일자_단순형("20060102", g.Date)
+		값.M지정일_주가 = lib.F2정수64_단순형(g.Tprice)
+		값.M지정일_대비_등락폭 = lib.F2정수64_단순형(g.Tchange)
+		값.M지정일_대비_등락율 = lib.F2실수_소숫점_추가_단순형(g.Tdiff, 2)
+		값.M사유 = T관리종목_지정_사유_구분(lib.F2정수64_단순형_공백은_0(g.Reason))
+		값.M해제일 = lib.F2포맷된_일자_단순형_공백은_초기값("20060102", g.Edate)
 
-		값.M배열[i] = s
+		값_모음.M배열[i] = 값
 	}
 
-	return 값, nil
+	return 값_모음, nil
 }
